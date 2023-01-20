bca8021ce00c6e688088c85261721b4339fcdacb
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-01-20 16:52:49 -0500

    more stuff

--- prac_.py
+++ prac_.py
@@ -1,4 +1,22 @@
 import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# print(torch.__version__)
 
 x = torch.rand(5,3)
-print(x)
+y = torch.tensor([7, 7])
+print(y.ndim)
+print(y.shape)
+
+MATRIX = torch.tensor([[7, 7], [9, 10]])
+
+print(MATRIX)
+print(MATRIX.ndim)
+print(MATRIX.shape)
+
+TENSOR = torch.tensor([[[4, 6, 2], [5, 9, 1], [9, 8, 7]]])
+
+print(TENSOR)
+print(TENSOR.ndim)
+print(TENSOR.shape)
