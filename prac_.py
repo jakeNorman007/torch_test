ad95af7a0c9139f68c68210e95292763e11f982e
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-01-26 16:03:25 -0500

    section 2 of course

--- prac_.py
+++ prac_.py
@@ -1,25 +1,18 @@
 import torch
-import pandas as pd
-import numpy as np
+from torch import nn
 import matplotlib.pyplot as plt
-# print(torch.__version__)
 
-# Tenosrs and Matrix are usually named in CAPS i.e TENSOR, MATRIX
+# data prep and loading
 
-# X = torch.rand(3, 4)
+weight = 0.7
+bias = 0.3
 
-# # print(X.ndim) # 2 in this case. number of dimensions can be determined by the number of indexes in the random tensor we created
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
 
-# # Creating a random tensor with a simillar shape to an image tensor
-# # name = torch.rand(size=(heigh, width, color channels(Red, Green, Blue)))
+X[:10], y[:10]
 
-# IMG_TENSOR = torch.rand(size=(224, 224, 3))
-# print(IMG_TENSOR.shape, IMG_TENSOR.ndim)
-
-
-MATRIX = torch.tensor([[1, 2, 3], [3, 1, 2]])
-MATRIX_2 = torch.tensor([[1, 3], [1, 1], [4, 5]])
-
-print(MATRIX @ MATRIX_2)
-
-#2:34:50 - time stamp for the course video
+# vid time stamp 4:36:43
