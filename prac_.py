47807c9516c7b407921cf5afb5119ea96df0fb3f
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-01-27 14:19:51 -0500

    small training with known data

--- prac_.py
+++ prac_.py
@@ -42,3 +42,22 @@ X_train, y_train = X[:train_split], y[:train_split] # trains on 80% of the data
 X_test, y_test = X[train_split:], y[train_split:] # tests on 20% of the data
 
 print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+def plot_predictions(train_data=X_train, train_labels=y_train, 
+                     test_data=X_test, test_labels=y_test, 
+                     predictions=None):
+  '''
+  Plots the trining data, test data and compares predictions.
+  '''
+  plt.figure(figsize=(10, 7))
+  # training data
+  plt.scatter(train_data, train_labels, c='b', s=4, label="Training data")
+  
+  # test data
+  plt.scatter(test_data, test_labels, c='g', s=4, label="Test data")
+  if predictions is not None:
+    plt.scatter(test_data, predictions, c='r', s=4, label="Predictions")
+    
+  plt.legend(prop={"size": 14})
+  
+plot_predictions
